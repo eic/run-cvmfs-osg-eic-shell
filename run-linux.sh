067c68b8c12ab80f613e708a38178f3b0fd0bc06
Wouter Deconinck <wdconinc@gmail.com>
2025-12-11 13:41:25 -0600

    Change installation method for Apptainer package

--- run-linux.sh
+++ run-linux.sh
@@ -59,7 +59,8 @@ for deb in "apptainer_${v/v/}_amd64.deb" "apptainer-suid_${v/v/}_amd64.deb"; do
     wget --tries 5 --output-document ${APPTAINER_DEB_CACHE}/${deb} https://github.com/apptainer/apptainer/releases/download/${v}/${deb}
   fi
   sudo rm -f /var/lib/man-db/auto-update
-  sudo apt install ${APPTAINER_DEB_CACHE}/${deb}
+  sudo mv ${APPTAINER_DEB_CACHE}/${deb} /var/cache/apt/archives/${deb}
+  sudo apt-get -q -y install /var/cache/apt/archives/${deb}
   sudo touch /var/lib/man-db/auto-update
 done
 echo "::endgroup::"
