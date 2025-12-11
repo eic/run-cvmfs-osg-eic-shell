1349401285a012a610aefdba75fe2cd3662325ec
Wouter Deconinck <wdconinc@gmail.com>
2025-12-11 13:22:19 -0600

    Change Apptainer installation path to use cache directory

--- run-linux.sh
+++ run-linux.sh
@@ -54,11 +54,11 @@ fi
 
 echo "::group::Installing Apptainer ${v}"
 for deb in "apptainer_${v/v/}_amd64.deb" "apptainer-suid_${v/v/}_amd64.deb"; do
-  if [ ! -s /var/cache/apt/archives/${deb} ] ; then
-    sudo wget --tries 5 --output-document /var/cache/apt/archives/${deb} https://github.com/apptainer/apptainer/releases/download/${v}/${deb}
+  if [ ! -s ${APPTAINER_DEB_CACHE}/${deb} ] ; then
+    sudo wget --tries 5 --output-document ${APPTAINER_DEB_CACHE}/${deb} https://github.com/apptainer/apptainer/releases/download/${v}/${deb}
   fi
   sudo rm -f /var/lib/man-db/auto-update
-  sudo apt-get -q -y install /var/cache/apt/archives/${deb}
+  sudo apt-get -q -y install ${APPTAINER_DEB_CACHE}/${deb}
   sudo touch /var/lib/man-db/auto-update
 done
 echo "::endgroup::"
